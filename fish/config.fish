3ae8a4cb053b4ea2165905c34a3a5c52c6a1267e
Arthurdw <dev@arthurdw.com>
2022-04-16 01:06:02 +0200

    :sparkles: Added bat alias

--- fish/config.fish
+++ fish/config.fish
@@ -1,59 +1,66 @@
 if status is-interactive
-	# Gets executed on shell startup
-	function fish_greeting
-	    fastfetch
-	end
+    # Gets executed on shell startup
+    function fish_greeting
+        # fastfetch
+        fastfetch --structure Title:Separator:OS:Kernel:Uptime:Packages:Shell:CPU:GPU:Memory:Disk:Battery:LocalIP
+    end
 
-	# Make a directory and go to it
-	function mkcd
-	    mkdir -p $argv && cd $argv
-	end
+    # Make a directory and go to it
+    function mkcd
+        mkdir -p $argv && cd $argv
+    end
 
     # Generate a random string x chars long
     alias rstr='openssl rand -hex'
 
-	# Vim aliases
+    # Vim aliases
     alias v='nvim'
-	alias vi='v'
-	alias vim='v'
-	alias vimtutor='v +Tutor'
+    alias vi='v'
+    alias vim='v'
+    alias vimtutor='v +Tutor'
 
-	# General aliases
-	alias cls='clear'
-	alias py='python3'
+    # General aliases
+    alias cls='clear'
+    alias py='python3'
 
-	# Git related aliases
-	alias gmc='gitmoji -c'
-	alias ghs='git status'
-	alias ghp='git push'
-	alias ghc='git commit -m'
-	alias gha='git add .'
-	alias gmca='gha && gmc'
-	alias ghg='git log --all --decorate --oneline --graph'
-	alias ghr='git reflog'
-	alias ghca='git commit --amend'
-	alias ghst='git stash'
-	alias ghsts='ghst show -p'
-	alias ghstss='ghst save'
-	alias ghstp='ghst pop'
-	alias ghstd='ghst drop'
-	alias ghc='git checkout'
-	alias ghcr='ghc --'
-	alias ght='git tag'
-	alias ghta='ght -a'
-	alias ghtd='ght -d'
+    # Git related aliases
+    alias gmc='gitmoji -c'
+    alias ghs='git status'
+    alias ghp='git push'
+    alias ghc='git commit -m'
+    alias gha='git add .'
+    alias gmca='gha && gmc'
+    alias ghg='git log --all --decorate --oneline --graph'
+    alias ghr='git reflog'
+    alias ghca='git commit --amend'
+    alias ghst='git stash'
+    alias ghsts='ghst show -p'
+    alias ghstss='ghst save'
+    alias ghstp='ghst pop'
+    alias ghstd='ghst drop'
+    alias ghc='git checkout'
+    alias ghcr='ghc --'
+    alias ght='git tag'
+    alias ghta='ght -a'
+    alias ghtd='ght -d'
     alias ghbc='git fetch -p && git branch -vv | awk "/: gone]/{print $1}" | xargs git branch -d'
 
-	# Aliases for non default applications
-	alias cat='batcat -p'
+    # Key counter aliases (https://github.com/arthurdw/keycounter)
+    alias __sortCSVBySecondValue="sort -n -k 2 -t ','"
+    alias kc='__sortCSVBySecondValue /etc/key-counter/data.csv'
+    alias kcc='__sortCSVBySecondValue /etc/key-counter/combinations.csv'
+
+    # Aliases for non default applications
+    alias batcat='bat'
+    alias cat='batcat -p'
     alias l='ls -alFhS'
-	alias ll='natls --long'
-	alias fire='DISPLAY= cacafire'
+    alias ll='natls --long'
+    alias fire='DISPLAY= cacafire'
 
-	# Config aliases
-	set FISH_PATH ~/.config/fish/config.fish
-	alias efish='vim $FISH_PATH && source $FISH_PATH'
+    # Config aliases
+    set FISH_PATH ~/.config/fish/config.fish
+    alias efish='vim $FISH_PATH && source $FISH_PATH'
 
-	# Set default editor to neovim
-	set -x EDITOR nvim
+    # Set default editor to neovim
+    set -x EDITOR nvim
 end
