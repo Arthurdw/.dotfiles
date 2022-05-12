352a623249f46441910406391c82e431b412e3d1
Arthurdw <dev@arthurdw.com>
2022-05-12 21:44:36 +0200

    :sparkles: Debugging util

--- nvim/lua/arthurdw/nvim-tree.lua
+++ nvim/lua/arthurdw/nvim-tree.lua
@@ -1,1 +1,12 @@
-require("nvim-tree").setup {}
+require("nvim-tree").setup {
+    auto_close = true,
+    diagnostics = {
+        enable = true,
+        icons = {
+            hint = "",
+            info = "",
+            warning = "",
+            error = ""
+        }
+    }
+}
