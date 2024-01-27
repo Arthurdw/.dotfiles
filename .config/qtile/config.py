24b0f5d9db4da778be7b880a59ab7dc829d394c3
Arthurdw <dev@arthurdw.com>
2024-01-27 11:41:31 +0100

    fix: resolved issues with qtile conf

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -66,30 +66,20 @@ keys = [
     Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
     Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
-    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+    Key([mod, "shift"], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
     # Unsplit = 1 window displayed, like Max layout, but still with
     # multiple stack panes
-    Key(
-        [mod, "shift"],
-        "Return",
-        lazy.layout.toggle_split(),
-        desc="Toggle between split and unsplit sides of stack",
-    ),
+    Key( [mod, "shift"], "Return", lazy.layout.toggle_split(), desc="Toggle between split and unsplit sides of stack"),
     Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
     # Toggle between different layouts as defined below
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
     Key([mod], "q", lazy.window.kill(), desc="Kill focused window"),
-    Key(
-        [mod],
-        "f",
-        lazy.window.toggle_fullscreen(),
-        desc="Toggle fullscreen on the focused window",
-    ),
+    Key( [mod], "f", lazy.window.toggle_fullscreen(), desc="Toggle fullscreen on the focused window"),
     Key([mod], "t", lazy.window.toggle_floating(), desc="Toggle floating on the focused window"),
     Key([mod, "control"], "r", lazy.reload_config(), lazy.spawn("polybar -r"), desc="Reload the config"),
-    Key([mod, "control"], "d", lazy.spawn(path.expanduser("~/.screenlayout/default.sh")), desc="Reload displays"),
+    Key([mod, "control"], "d", lazy.spawn(path.expanduser( "~/.screenlayout/default.sh")), desc="Reload displays"),
     Key([mod, "control"], "q", lazy.pywalshutdown(), desc="Shutdown Qtile"),
     Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
     Key([mod], "b", lazy.spawn(browser), desc="Spawn browser"),
@@ -118,7 +108,8 @@ for i in groups:
                 [mod, "shift"],
                 i.name,
                 lazy.window.togroup(i.name, switch_group=True),
-                desc="Switch to & move focused window to group {}".format(i.name),
+                desc="Switch to & move focused window to group {}".format(
+                    i.name),
             ),
             # Or, use below if you prefer not to switch to that group.
             # # mod1 + shift + letter of group = move focused window to group
@@ -128,7 +119,7 @@ for i in groups:
     )
 
 layouts = [
-    layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
+    layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=2),
     layout.Max(),
     # Try more layouts by unleashing below layouts.
     # layout.Stack(num_stacks=2),
@@ -153,11 +144,12 @@ extension_defaults = widget_defaults.copy()
 wallpapers = glob(path.expanduser("~/Pictures/wallpapers/*"))
 wallpaper = wallpapers[randint(0, len(wallpapers) - 1)]
 
-subprocess.run(["cp", wallpaper, path.expanduser("~/.cache/current-wallpaper")])
+subprocess.run(
+    ["cp", wallpaper, path.expanduser("~/.cache/current-wallpaper")])
 
-screen_amount = [
+screens = [
     *[
-        Screen( 
+        Screen(
             wallpaper=wallpaper,
             wallpaper_mode="fill"
         ) for i in range(0, screen_amount)
