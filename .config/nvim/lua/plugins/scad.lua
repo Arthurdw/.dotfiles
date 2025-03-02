63c0423dc143aea92cda6f2dbabd2dda517d1b8d
Arthurdw <dev@arthurdw.com>
2025-03-02 12:35:02 +0100

    fix: update deps and proper scad plugin loading

--- .config/nvim/lua/plugins/scad.lua
+++ .config/nvim/lua/plugins/scad.lua
@@ -4,7 +4,17 @@ return {
     config = function()
       vim.g.openscad_load_snippets = true
       require("openscad")
+      vim.keymap.set(
+        "n",
+        "<leader>o",
+        "<cmd>lua require('openscad').exec_openscad()<CR>",
+        { noremap = true, silent = true }
+      )
     end,
-    dependencies = { "L3MON4D3/LuaSnip", "junegunn/fzf.vim" },
+    dependencies = {
+      "L3MON4D3/LuaSnip",
+      "junegunn/fzf.vim",
+    },
+    ft = { "openscad" },
   },
 }
